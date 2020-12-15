7eed8aa2ebfe01b5b962366273e24162965517fa
Martin Takac <martin.taki@gmail.com>
2020-12-15 13:48:51 -0500

    migration

---
+++ linky.php
@@ -0,0 +1,38 @@
+<div id="text">
+<h1>Partneri</h1>
+
+<ul>
+<li>
+<a href="http://www.hotelmajpiestany.cz/">Lázne Piešťany</a>- v hotelu Máj Piešťany.
+</li>
+<li>
+<a href="http://www.hotel-kosice.sk/">Hotel Košice</a>- ubytovanie v Košiciach rýchlo a jednoducho.
+</li>
+<li>
+<a href="http://www.liecebne-pobyty.sk/">Liečebné pobyty</a> na Slovensku v najlepších ubytovacích a liečebných zariadeniach.
+
+</li>
+<li>
+<a href="http://www.seniorske-pobyty.sk/">Seniorské pobyty</a> po celom Slovensku.
+</li>
+<li>
+<a href="http://www.relaxacne-pobyty.sk/">Wellness pobyty</a> na Slovensku.
+</li>
+<li>
+<a href="http://www.ubytovanie-chorvatsko-vir.sk/">Ubytovanie Chorvatsko</a>- letná dovolenka pri mori.
+</li>
+
+
+</ul>
+
+<p>&nbsp;</p>
+<p>&nbsp;</p>
+<p>&nbsp;</p><p>&nbsp;</p><p>&nbsp;</p> <p>&nbsp;</p>
+
+
+
+
+
+
+
+</div>
